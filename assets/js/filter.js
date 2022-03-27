d3e50d35e494e46d9298c70a3e78935a90e3728c
EvelinaYaln <evelina.dranets.com>
2022-03-27 21:41:49 +0300

    add css for cards

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -1,10 +1,10 @@
 let json = `[{
-        "name": "Золотое кольцо Абхазии",
+        "name": "Золотое кольцо Абхазии (из Адлера)",
         "code": "gold",
         "category": "plane",
-        "priceadult": "2000",
-        "pricechild": "1000",
-        "duration":"2",
+        "priceadult": "2 000",
+        "pricechild": "1 000",
+        "duration":"2 часа",
         "description":"очень красиво",
         "favorite":"0",
         "capacity":"",
@@ -15,9 +15,9 @@ let json = `[{
         "name": "Золотое кольцо Грузии",
         "code": "gold",
         "category": "bus",
-        "priceadult": "2000",
-        "pricechild": "1000",
-        "duration":"2",
+        "priceadult": "2 000",
+        "pricechild": "1 000",
+        "duration":"2 часа",
         "description":"очень красиво",
         "favorite":"0",
         "capacity":"",
@@ -28,9 +28,9 @@ let json = `[{
         "name": "Золотое кольцо Армении",
         "code": "gold",
         "category": "foot",
-        "priceadult": "2000",
-        "pricechild": "1000",
-        "duration":"2",
+        "priceadult": "2 000",
+        "pricechild": "1 000",
+        "duration":"2 часа",
         "description":"очень красиво",
         "favorite":"0",
         "capacity":"",
@@ -39,22 +39,39 @@ let json = `[{
         "image": "assets/img/3.jpeg"
     }]`
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let excursions = JSON.parse(json);
     let excursionsContent = "";
     for (let excursion of excursions) {
         excursionsContent +=
             `<div class="excursion_item">
+                <div class= "container_img">
                     <img class="characterImage" src="${excursion.image}" alt="${excursion.code}"></img>
+                </div>
+                <div class= "container_card">
+                    <p class= "tour_name">Автобусный тур</p>
                     <h2 class="characterName" id="${excursion.code}">${excursion.name}</h2>
                     <div class="excursionInfo">
-                        <div> ${excursion.priceadult}</div>
-                        <div> ${excursion.pricechild}</div>
-                        <div> ${excursion.duration}</div>
-                        <div> ${excursion.description}</div>
-                        <div> ${excursion.favorite}</div>
+                        <div class= "container_for_price">
+                            <i class="fa-solid fa-coins"></i>
+                            <div class= "priceadult"> ${excursion.priceadult}</div>
+                            <i class="fa-solid fa-coins"></i>
+                            <div class= "pricechild"> ${excursion.pricechild}</div>
+                            <i class="fa-regular fa-clock"></i>
+                            <div class= "duration"> ${excursion.duration}</div>
+                        </div>
+                        <div class= "container_for_ticket">
+                            <p class= "adult">Взрослый билет</p>
+                            <p class= "child">Детский билет</p>
+                        </div>    
+                        <div class="description"> ${excursion.description}</div>
+                        <div class="container">
+                            <a class="more" href="#">Подробнее</a>
+                            <button class="click"><i class="fa-regular fa-heart"></i></button>
+                        </div>
                     </div>
-                </div>`
+                </div>
+            </div>`
     }
 
     document.getElementById("excursions_container").innerHTML = excursionsContent;
