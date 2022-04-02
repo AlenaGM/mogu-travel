05ccc2b504ad2aceec7528518523b26e1b791bc6
AlenaGM <alena.guillaume4@gmail.com>
2022-04-02 17:37:47 +0200

    upd header burger

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -14,3 +14,31 @@ const productContainers = [...document.querySelectorAll('.product-container')];
                 item.scrollLeft -= containerWidth;
             })
         })
+
+//МЕНЮ-БУРГЕР
+
+const burgerMenu = document.querySelector('.menu__burger');
+const bodyMenu = document.querySelector('.menu__body');
+const menuLinks = document.querySelectorAll('.menu__link')
+
+if (burgerMenu) {
+    burgerMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        burgerMenu.classList.toggle('_active');
+        bodyMenu.classList.toggle('_active');
+    });
+}
+
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    });
+
+    function onMenuLinkClick() {
+        if (burgerMenu.classList.contains('_active')) {
+            document.body.classList.remove('_lock');
+            burgerMenu.classList.remove('_active');
+            bodyMenu.classList.remove('_active');
+        }
+    }
+}
