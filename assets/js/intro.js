48c4a6447fe2567f8ed4ad423f368ef805949798
VNeskoromnaya <neskoromnaya.v@gmail.com>
2022-04-02 23:06:35 +0300

    js for form

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,1 +1,57 @@
-console.log('intro');
+// --- JS код ддя раздела "Остались вопросы?" --- //
+const form = document.getElementById('form');
+const userName = document.getElementById('userName');
+const phone = document.getElementById('phone');
+const email = document.getElementById('email');
+const submit = document.getElementById('btn');
+
+
+
+function check() { 
+    // очистка сообщений об ошибках
+    let errorMessages = document.querySelectorAll('.error');
+    for (let error of errorMessages) {
+        error.style.display = 'none';
+    }
+
+    // проверка валидности введеной информации в input
+    let isValid = true; // валидна ли форма 
+
+    if(!userName.checkValidity()) {
+        isValid = false;
+        document.getElementsByClassName('userNameError')[0].style.display = 'block';
+    }
+
+    if(!phone.checkValidity()) {
+        isValid = false;
+        document.getElementsByClassName('phoneError')[0].style.display = 'block';
+    }
+
+    if(!email.checkValidity()) {
+        isValid = false;
+        document.getElementsByClassName('emailError')[0].style.display = 'block';
+    }  
+    
+    return isValid;
+}
+
+
+userName.addEventListener('blur', check);
+phone.addEventListener('blur', check);
+email.addEventListener('blur', check);
+
+
+submit.addEventListener('click', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    let isValid = check();
+    if (isValid) {
+        alert(`Спасибо! Мы свяжемся с Вами в ближайшее время`);
+        form.reset();
+    }
+});
+
+
+
+// -----------------------------------------------------------------
